6933069f299f480bfced20fa92d2fb88bc59520f
Hristov <danail.hristov@kpn.com>
2018-01-18 12:18:55 +0100

    first R file and README.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 4GettingAndCleanigDataProjectAssignment
+
+The raw data files used in the assignment can be found on this [link](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+The README.txt file provides detail explanation of the 
+## Assignment Goals:
+1. A README.md - The file  that explains the analysis files.(For information regarding the raw data files please refer to README.txt in [link](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).)
+2. CodeBook.md - The file describes the variables, the data, and any transformations or work performed to clean up the data.
+3. A run_analysis.R that does the following:
+*Merges the training and the test sets to create one data set.
+*Extracts only the measurements on the mean and standard deviation for each measurement.
+*Uses descriptive activity names to name the activities in the data set
+*Appropriately labels the data set with descriptive variable names.
+*From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,38 @@
 
+setwd("C:/Users/hrist499/Documents/Coursera/DataSciennce/4GettingAndCleanigDataProjectAssignment/")
+library(dplyr)
+###READ IN THE DATA FILES
 subjectTest<-read.csv("../../UCI HAR Dataset/test/subject_test.txt",header = FALSE)
 XTest<-read.csv("../../UCI HAR Dataset/test/X_test.txt",sep ="",header = FALSE)
 YTest<-read.csv("../../UCI HAR Dataset/test/y_test.txt",header = FALSE)
 subjectTrain<-read.csv("../../UCI HAR Dataset/train/subject_train.txt",header = FALSE)
 XTrain<-read.csv("../../UCI HAR Dataset/train/X_train.txt",sep ="",header = FALSE)
 YTrain<-read.csv("../../UCI HAR Dataset/train/y_train.txt",header = FALSE)
-innertial<-read.csv("../../UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt",sep ="",header = FALSE)
-mean((innertial[1,]))
-vars <- readLines("../../UCI HAR Dataset/features.txt")
+###READ IN THE ACTIVITY NAMES
+activities<-read.csv("../../UCI HAR Dataset/activity_labels.txt",sep="",header = FALSE)
 
-varNames <- gsub([^A-Za-z|0-9],"",x=vars)
-v<- read.csv("../../UCI HAR Dataset/features.txt",sep="",header = FALSE)
-train <- cbind(subjectTrain,XTrain,YTrain)
-test <- cbind(subjectTest,XTest,YTest)
-XTest[1,]
-grep(-0.004203632,XTest[,1])
+
+###READ IN THE VARIABLE NAMES
+vars<- read.csv("../../UCI HAR Dataset/features.txt",sep="",header = FALSE)
+varNames <- gsub("[^A-Za-z|0-9]","",x=vars$V2)
+
+### COMBINE DATA
+train <- cbind(subjectTrain,YTrain,XTrain)
+test <- cbind(subjectTest,YTest,XTest)
+dataSet<-rbind(train,test)
+###ASSIGN COLUMN NAMES
+names(dataSet)<- c("subject","activity",varNames)
+
+###EXTRACT DATA FOR mean AND std
+### get the column for the users and activities and all that contain "mean" and "std" in their name
+
+meanStdData <-dataSet[,c(1,2,grep("mean|std",names(dataSet)))]
+
+###CHANGE THE ACTIVITY VARIABLE
+meanStdData$activity<-as.factor(meanStdData$activity)
+levels(meanStdData$activity)<-activities[,2]
+
+### CONVERT TO DPLYR FORMAT
+dataSet_Dp <- tbl_df(meanStdData)
+
+summaryData <- dataSet_Dp %>% group_by(subject,activity) %>% summarise_all(funs(mean))
